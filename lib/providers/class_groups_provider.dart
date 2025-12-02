72dc5673bfecc28d11e55e98acbcf8d3861de729
wenqingNie <1258820178@qq.com>
2025-12-02 19:47:03 +0800

    解决折叠展开崩溃bug

--- lib/providers/class_groups_provider.dart
+++ lib/providers/class_groups_provider.dart
@@ -3,57 +3,63 @@ import 'package:flutter/material.dart';
 import '../models/student_class_group.dart';
 
 class ClassGroupsProvider with ChangeNotifier {
-  final List<StudentClassGroup> _classGroups = [];
-  final List<StudentClassGroup> _filterClassGroups = [];
+  // final List<StudentClassGroup> _classGroups = [];
+  // final List<StudentClassGroup> _filterClassGroups = [];
+  final Map<String, StudentClassGroup> _classGroups = {};
+  final Map<String, StudentClassGroup> _filterClassGroups = {};
 
-  List<StudentClassGroup> get classGroups => _classGroups;
-  List<StudentClassGroup> get filterClassGroups => _filterClassGroups;
+  List<StudentClassGroup> get classGroups => _classGroups.values.toList();
+  List<StudentClassGroup> get filterClassGroups =>
+      _filterClassGroups.values.toList();
 
   void addClassGroupWithoutNotify(StudentClassGroup group) {
-    _classGroups.add(group);
+    _classGroups.putIfAbsent(group.studentClass.className, () => group);
   }
 
   void changeClassGroups(List<StudentClassGroup> newList) {
-    var isExpands = _classGroups.map((e) => e.isExpanded).toList();
+    var tClassGroups = Map.from(_classGroups);
     _classGroups.clear();
-    _classGroups.addAll(newList);
-    for (int i = 0; i < _classGroups.length; i++) {
-      _classGroups[i].isExpanded = isExpands[i];
+    for (var group in newList) {
+      if (tClassGroups.containsKey(group.studentClass.className)) {
+        group.isExpanded =
+            tClassGroups[group.studentClass.className]!.isExpanded;
+      }
+      _classGroups.putIfAbsent(group.studentClass.className, () => group);
     }
     notifyListeners();
   }
 
   void changeClassGroupsWithoutNotify(List<StudentClassGroup> newList) {
-    var isExpands = _classGroups.map((e) => e.isExpanded).toList();
+    var tClassGroups = Map.from(_classGroups);
     _classGroups.clear();
-    _classGroups.addAll(newList);
-    for (int i = 0; i < _classGroups.length; i++) {
-      _classGroups[i].isExpanded = isExpands[i];
+    for (var group in newList) {
+      if (tClassGroups.containsKey(group.studentClass.className)) {
+        group.isExpanded =
+            tClassGroups[group.studentClass.className]!.isExpanded;
+      }
+      _classGroups.putIfAbsent(group.studentClass.className, () => group);
     }
   }
 
   void changeFilterClassGroupsWithoutNotify(String filter) {
     _filterClassGroups.clear();
-    _filterClassGroups.addAll(
-      _classGroups
-          .where((element) => element.studentClass.className.contains(filter))
-          .toList(),
-    );
+    for (var group in _classGroups.entries) {
+      _filterClassGroups.putIfAbsent(group.key, () => group.value);
+    }
   }
 
   void changeFilterClassGroups(String filter) {
     _filterClassGroups.clear();
-    _filterClassGroups.addAll(
-      _classGroups,
-      //     .where((element) => element.studentClass.className.contains(filter))
-      //     .toList(),
-    );
+    for (var group in _classGroups.entries) {
+      _filterClassGroups.putIfAbsent(group.key, () => group.value);
+    }
     notifyListeners();
   }
 
   void changeExpanded(int index) {
-    _filterClassGroups[index].isExpanded =
-        !_filterClassGroups[index].isExpanded;
+    var aList = _filterClassGroups.values.toList();
+    _classGroups[aList[index].studentClass.className]!.isExpanded =
+        !aList[index].isExpanded;
     notifyListeners();
   }
 }
