ea819d1b6110521b6d315912feb9275fc874f561
wenqingNie <1258820178@qq.com>
2025-12-15 19:35:07 +0800

    解决刚开始没有主题数据错误

--- lib/providers/them_switcher_provider.dart
+++ lib/providers/them_switcher_provider.dart
@@ -7,7 +7,8 @@ import 'package:flutter/material.dart'
         TextStyle,
         TextTheme,
         ThemeData,
-        ThemeMode;
+        ThemeMode,
+        Colors;
 import 'package:flutter_screenutil/flutter_screenutil.dart' show SizeExtension;
 
 import '../configs/theme_style_option_enum.dart';
@@ -31,25 +32,158 @@ class ThemeSwitcherProvider extends ChangeNotifier {
     labelSmall: TextStyle(fontSize: 16.sp),
   );
 
+  final _themeDataMap = {
+    ThemeStyleOption.blue: {
+      Brightness.light: ThemeData(
+        brightness: Brightness.light,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.light,
+        ),
+        textTheme: _textTheme,
+      ),
+      Brightness.dark: ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.dark,
+        ),
+        textTheme: _textTheme,
+      ),
+    },
+    ThemeStyleOption.red: {
+      Brightness.light: ThemeData(
+        brightness: Brightness.light,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.red,
+          brightness: Brightness.light,
+        ),
+        textTheme: _textTheme,
+      ),
+      Brightness.dark: ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.red,
+          brightness: Brightness.dark,
+        ),
+        textTheme: _textTheme,
+      ),
+    },
+    ThemeStyleOption.green: {
+      Brightness.light: ThemeData(
+        brightness: Brightness.light,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.green,
+          brightness: Brightness.light,
+        ),
+        textTheme: _textTheme,
+      ),
+      Brightness.dark: ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.green,
+          brightness: Brightness.dark,
+        ),
+        textTheme: _textTheme,
+      ),
+    },
+    ThemeStyleOption.yellow: {
+      Brightness.light: ThemeData(
+        brightness: Brightness.light,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.yellow,
+          brightness: Brightness.light,
+        ),
+        textTheme: _textTheme,
+      ),
+      Brightness.dark: ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.yellow,
+          brightness: Brightness.dark,
+        ),
+        textTheme: _textTheme,
+      ),
+    },
+    ThemeStyleOption.purple: {
+      Brightness.light: ThemeData(
+        brightness: Brightness.light,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple,
+          brightness: Brightness.light,
+        ),
+        textTheme: _textTheme,
+      ),
+      Brightness.dark: ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple,
+          brightness: Brightness.dark,
+        ),
+        textTheme: _textTheme,
+      ),
+    },
+    ThemeStyleOption.orange: {
+      Brightness.light: ThemeData(
+        brightness: Brightness.light,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.orange,
+          brightness: Brightness.light,
+        ),
+        textTheme: _textTheme,
+      ),
+      Brightness.dark: ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.orange,
+          brightness: Brightness.dark,
+        ),
+        textTheme: _textTheme,
+      ),
+    },
+    ThemeStyleOption.indigo: {
+      Brightness.light: ThemeData(
+        brightness: Brightness.light,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.indigo,
+          brightness: Brightness.light,
+        ),
+        textTheme: _textTheme,
+      ),
+      Brightness.dark: ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.indigo,
+          brightness: Brightness.dark,
+        ),
+        textTheme: _textTheme,
+      ),
+    },
+    ThemeStyleOption.diy: {
+      Brightness.light: ThemeData(
+        brightness: Brightness.light,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: ThemeStyleOption.diy.color,
+          brightness: Brightness.light,
+        ),
+        textTheme: _textTheme,
+      ),
+      Brightness.dark: ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: ThemeStyleOption.diy.color,
+          brightness: Brightness.dark,
+        ),
+        textTheme: _textTheme,
+      ),
+    },
+  };
+
   ThemeMode get themeMode => _themeMode;
   ThemeStyleOption get themeStyle => _themeStyle;
 
-  ThemeData get theme => ThemeData(
-    brightness: Brightness.light,
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: _themeStyle.color,
-      brightness: Brightness.light,
-    ),
-    textTheme: _textTheme,
-  );
-  ThemeData get darkTheme => ThemeData(
-    brightness: Brightness.dark,
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: _themeStyle.color,
-      brightness: Brightness.dark,
-    ),
-    textTheme: _textTheme,
-  );
+  ThemeData get theme => _themeDataMap[themeStyle]![Brightness.light]!;
+  ThemeData get darkTheme => _themeDataMap[themeStyle]![Brightness.dark]!;
 
   void setThemeMode(ThemeMode themeMode) {
     _themeMode = themeMode;
@@ -57,13 +191,39 @@ class ThemeSwitcherProvider extends ChangeNotifier {
   }
 
   void setThemeStyle(ThemeStyleOption themeStyle) {
+    if (themeStyle == ThemeStyleOption.diy) {
+      _updateDiyThemeData();
+    }
     _themeStyle = themeStyle;
     notifyListeners();
+  }
+
+  void _updateDiyThemeData() {
+    if (_themeStyle != ThemeStyleOption.diy) return;
+    _themeDataMap[ThemeStyleOption.diy]![Brightness.light] = ThemeData(
+      brightness: Brightness.light,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: ThemeStyleOption.diy.color,
+        brightness: Brightness.light,
+      ),
+      textTheme: _textTheme,
+    );
+    _themeDataMap[ThemeStyleOption.diy]![Brightness.dark] = ThemeData(
+      brightness: Brightness.dark,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: ThemeStyleOption.diy.color,
+        brightness: Brightness.dark,
+      ),
+      textTheme: _textTheme,
+    );
   }
 
   void setModelAndStyle(ThemeMode themeMode, ThemeStyleOption themeStyle) {
     _themeMode = themeMode;
     _themeStyle = themeStyle;
+    if (themeStyle == ThemeStyleOption.diy) {
+      _updateDiyThemeData();
+    }
 
     notifyListeners();
   }
